fe082fc2bad2d6f70063a7aa7c1a450db7202b10
Shaurya144 <chundawatshaurya@gmail.com>
2024-07-14 20:16:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # These are all the data types in Python
 
 x = "Hello World"	                            # str	
-x = 20	                                        # int	
+x = 20	                                      # int	
 x = 20.5	                                    # float	
-x = 1j	                                        # complex	
+x = 1j	                                      # complex	
 x = ["apple", "banana", "cherry"]	            # list	
 x = ("apple", "banana", "cherry")	            # tuple	
 x = range(6)	                                # range	
